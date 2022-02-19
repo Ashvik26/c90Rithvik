b1e29ed2a3751defe690e0fd7e534aedff2000f5
Ashvik26 <84242727+Ashvik26@users.noreply.github.com>
2022-02-19 12:47:48 -0500

    Add files via upload

---
+++ gamepage.js
@@ -0,0 +1,10 @@
+player1_name=localStorage.getItem("player1_name");
+player2_name=localStorage.getItem("player2_name");
+var player1_score=0;
+var player2_score=0;
+document.getElementById("player1_name").innerHTML=player1_name;
+document.getElementById("player2_name").innerHTML=player2_name;
+document.getElementById("player1_score").innerHTML=player1_score;
+document.getElementById("player2_score").innerHTML=player2_score;
+document.getElementById("question_turn").innerHTML="question turn:"+player1_name;
+document.getElementById("answer_turn").innerHTML="answer turn:"+player2_name;
